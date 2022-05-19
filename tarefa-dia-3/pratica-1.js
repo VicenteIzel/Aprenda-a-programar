3bb02f84be881a5ab2e9f0316382a5485d73055c
VicenteIzel <vicente.izel@gmail.com>
2022-05-19 10:32:10 -0300

    primeira parte da tarefa 1

---
+++ tarefa-dia-3/pratica-1.js
@@ -0,0 +1,22 @@
+console.log("Tarefa do dia 3")
+
+//variáveis
+let nome
+let email
+let profissão
+let idade
+let lugar
+
+//atribuir os valores
+nome = "Nome: Vicente Augusto"
+console.log(nome)
+email = "Email: vicente.izel@gmail.com"
+console.log(email)
+profissão = "Profissão = Autônomo"
+console.log(profissão)
+idade = "Idade: 27"
+console.log(idade)
+lugar = "o lugar que você sonha um dia conhecer: Liverpool"
+console.log(lugar)
+
+//Fim das atribuições
